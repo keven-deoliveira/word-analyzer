a385f33776b978d7b4df83992894390f69a6cf64
keven-deoliveira <kevend@bu.edu>
2022-01-20 11:25:42 -0500

    Initial stub functions for Phase 1

---
+++ main.py
@@ -0,0 +1,13 @@
+# parse document, return dictionary of words and their count
+def parse(document):
+    pass
+
+# takes dictionary as input and creates histogram
+def histogram(wordcount):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
