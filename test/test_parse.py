ba5c433d43b9c0daa998f5e583ae6a15adddf3d1
keven-deoliveira <kevend@bu.edu>
2022-01-25 15:32:28 -0500

    Updated test file and cases for parse function

--- test/test_parse.py
+++ test/test_parse.py
@@ -1,7 +1,20 @@
 def parse(document):
     wordcount = {}
+
+    # with NLTK, might have to replace ' with a space or something
+    NLTK = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", 
+    "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", 
+    "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", 
+    "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", 
+    "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", 
+    "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", 
+    "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", 
+    "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
     for word in document.split():
-        if word not in wordcount:
+        if word in NLTK:
+            continue
+        elif word not in wordcount:
             wordcount[word] = 1
         else:
             wordcount[word] += 1
@@ -9,10 +22,12 @@ def parse(document):
     return wordcount
 
 
-test_str = "hello hello hello\nhello testing test test\ntest\ntest me me me me me again"
+test_str = "hello hello\nhello test test\ntest me me me me me again"
 
 
 def test_parse():
-    assert parse(test_str) is not None  # should not be empty / should return something
-    assert isinstance(parse(test_str), dict)  # should return a dictionary
-    assert ("me" not in parse(test_str)) and ("again" not in parse(test_str))  # NLTK works should not be in dictionary
+    wordcount = parse(test_str)
+    assert wordcount is not None  # should not be empty / should return something
+    assert isinstance(wordcount, dict)  # should return a dictionary
+    assert "me" not in wordcount  # NLTK works should not be in dictionary
+    assert "again" not in wordcount  # NLTK works should not be in dictionary
