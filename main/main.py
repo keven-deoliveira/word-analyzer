dff99bded060baf772e88c143e380e091c15834b
keven-deoliveira <kevend@bu.edu>
2022-01-25 15:06:25 -0500

    Added functionality to parse function to ignore NLTK words

--- main/main.py
+++ main/main.py
@@ -5,11 +5,23 @@ import matplotlib.pyplot as plt
 # parse document (string), return dictionary of words and their count
 def parse(document):
     wordcount = {}
+
+    NLTK = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", 
+    "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", 
+    "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", 
+    "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", 
+    "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", 
+    "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", 
+    "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", 
+    "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
     for word in document.split():
-        if word not in wordcount:
+        if (word not in wordcount) and (word not in NLTK):
             wordcount[word] = 1
-        else:
+        elif word not in NLTK:
             wordcount[word] += 1
+        else:
+            pass
 
     return wordcount
 
