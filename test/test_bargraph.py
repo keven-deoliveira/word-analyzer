594ad76d50847004000655c2961a510b0d97d664
keven-deoliveira <kevend@bu.edu>
2022-01-25 09:12:32 -0500

    Fixed styling issues caught by linter

--- test/test_bargraph.py
+++ test/test_bargraph.py
@@ -14,4 +14,4 @@ wordcount = {"hello": 4, "test": 2, "color": 6, "histogram": 1, "software": 11}
 
 
 def test_histogram():
-    assert barGraph(wordcount) is None  #should not return anything
+    assert barGraph(wordcount) is None  # should not return anything
