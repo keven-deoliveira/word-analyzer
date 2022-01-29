752f441268467352edbd7d79278c61873da9ef3e
keven-deoliveira <kevend@bu.edu>
2022-01-28 20:54:25 -0500

    created new scrub function to check for special characters

--- main/functions.py
+++ main/functions.py
@@ -1,17 +1,30 @@
+from tokenize import Special
 import matplotlib.pyplot as plt
+from string import punctuation
+
+
+def scrub(word):
+    copy = word
+    special_chars = list(punctuation)
+
+    for char in copy:
+        if char in special_chars:
+            copy = copy.replace(char, "")
+
+    return copy
 
 
 def checkNLTK(word):
     NLTK = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours",
             "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself",
-            "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", 
+            "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which",
             "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be",
-            "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", 
-            "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", 
-            "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", 
+            "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an",
+            "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for",
+            "with", "about", "against", "between", "into", "through", "during", "before", "after", "above",
             "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further",
-            "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", 
-            "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", 
+            "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few",
+            "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too",
             "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 
     if "'" in word:
@@ -19,7 +32,7 @@ def checkNLTK(word):
 
     word_split = word.split()
 
-    if word_split[0] in NLTK:
+    if any(x in word_split for x in NLTK):
         return True
 
     return False
@@ -28,13 +41,15 @@ def checkNLTK(word):
 # parse document (string), return dictionary of words and their count
 def parse(document):
     wordcount = {}
-    
+
     for word in document.split():
         if checkNLTK(word):
             continue
         elif word not in wordcount:
+            word = scrub(word)
             wordcount[word] = 1
         else:
+            word = scrub(word)
             wordcount[word] += 1
 
     return wordcount
